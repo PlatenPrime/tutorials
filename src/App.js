5f9188d285b0847ae6b348c28f30effc2b0b95a0
PlatenPrime <proshta@gmail.com>
2022-07-04 08:42:08 +0300

    Added simplest routing and first page for practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import { Link } from "react-router-dom";
+import { Outlet } from 'react-router';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-
+			<Link to="mui"><h3>Go to MUI page</h3></Link>
+			<Outlet />
 		</div>
 	);
 }
---
+++ src/pages/MUI/MUI.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MUI = () => {
+	return (
+		<div>
+			<h1>This is an incredible page to practice with MUI library</h1>
+		</div>
+	);
+};
+
+export default MUI;
