fe2f7f7f55e46cd41335b7585b8f4ec8d67dc5ce
PlatenPrime <proshta@gmail.com>
2022-07-07 21:06:48 +0300

    Update MUINavBar.jsx

--- src/pages/MUI/MUIComponents/MUINavBar.jsx
+++ src/pages/MUI/MUIComponents/MUINavBar.jsx
@@ -1,10 +1,58 @@
-import { AppBar, Toolbar } from '@mui/material';
+import { AppBar, Box, InputBase, styled, Toolbar, Typography } from '@mui/material';
 import React from 'react';
+import CastForEducationIcon from '@mui/icons-material/CastForEducation';
+
+
+
+const StyledToolbar = styled(Toolbar)({
+	display: "flex",
+	justifyContent: "space-between",
+});
+
+
+const Search = styled("div")(({ theme }) => ({
+	backgroundColor: "white",
+	padding: "0 10px",
+	borderRadius: theme.shape.borderRadius,
+	width: "40%",
+
+
+}));
+
+
+const Icons = styled(Box)(({ theme }) => ({
+	backgroundColor: "white",
+
+}));
+
 
 const MUINavBar = () => {
 	return (
-		<AppBar position='relative'>
-			<Toolbar>NavBar</Toolbar>
+		<AppBar position='sticky'>
+			<StyledToolbar>
+				<Typography variant='h6'
+					sx={{
+						display: {
+							xs: "none",
+							sm: "block"
+						}
+					}}
+				>
+					Platen
+				</Typography>
+				<CastForEducationIcon sx={{
+					display: {
+						xs: "block",
+						sm: "none"
+					}
+				}} />
+				<Search>
+					<InputBase placeholder='Search...' />
+				</Search>
+				<Icons>
+					Icons
+				</Icons>
+			</StyledToolbar>
 		</AppBar>
 	);
 };
