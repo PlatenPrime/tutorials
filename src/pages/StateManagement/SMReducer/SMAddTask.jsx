057bb28b70b3b75ec5fa016364bf9ee26dafbeae
PlatenPrime <proshta@gmail.com>
2022-07-26 20:47:41 +0300

    reducer

---
+++ src/pages/StateManagement/SMReducer/SMAddTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SMAddTask = () => {
+	return (
+		<div>
+			
+		</div>
+	);
+};
+
+export default SMAddTask;
---
+++ src/pages/StateManagement/SMReducer/SMTaskList.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import SMAddTask from './SMAddTask';
+
+
+
+
+
+
+
+
+
+
+
+
+const SMTaskList = () => {
+
+
+
+
+
+	return (
+
+
+
+
+		<>
+			<h1>Prague itinerary</h1>
+			
+		</>
+
+
+
+
+
+
+
+
+
+
+
+	);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default SMTaskList;
