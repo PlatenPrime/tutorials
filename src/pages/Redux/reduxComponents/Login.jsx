9cbc729060eb14af9e373f8fbe87bf8c4957e585
PlatenPrime <proshta@gmail.com>
2022-08-02 20:27:12 +0300

    continue with Pedro redux tutorial

--- src/pages/Redux/reduxComponents/Login.jsx
+++ src/pages/Redux/reduxComponents/Login.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import { useDispatch } from "react-redux";
+import { login, logout } from "./../reduxFeatures/user";
+
 const Login = () => {
+
+	const dispatch = useDispatch()
+
+
 	return (
 		<div>
-			<button>Login</button>
+			<button
+				onClick={() => { dispatch(login({ name: "Prime", age: 92, email: "platenprime@gmail.com" })) }}
+			>
+				Login
+			</button>
+
+			<button
+				onClick={() => { dispatch(logout()) }}
+			>
+				LogOut
+			</button>
 		</div>
 	);
 };
--- src/pages/Redux/reduxFeatures/user.js
+++ src/pages/Redux/reduxFeatures/user.js
@@ -1,15 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
+const initialStateValue = { name: "Platen", age: 29, email: "proshta@gmail.com" };
+
+
 export const userSlice = createSlice({
 	name: "user",
-	initialState: { value: { name: "Platen", age: 29, email: "proshta@gmail.com" } },
+	initialState: { value: initialStateValue },
 	reducers: {
 		login: (state, action) => {
 			state.value = action.payload;
 		},
+		logout: (state) => {
+			state.value = initialStateValue;
+		}
 	},
 });
+
+
+
+export const { login, logout } = userSlice.actions;
 
 
 export default userSlice.reducer;
